1cf5f381aae26f541b90e26d856406536e9758d6
Reti97 <73790811+Reti97@users.noreply.github.com>
2020-11-28 22:52:37 +0100

    Greenwashed Dashboard woohoo ^^

--- www/app/js/demo/chart-pie-demo.js
+++ www/app/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Ökologisch", "Schnell"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [65, 35],
+      backgroundColor: ['#56A207', '#E15813',],
+      hoverBackgroundColor: ['#25C40C', '#E18713'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
